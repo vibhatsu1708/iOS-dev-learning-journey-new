25fb7c21e875156344ce478f0697c4723b8a5b5e
vibhatsu08 <mistryvedant17@gmail.com>
2023-10-06 04:13:49 +0530

    Learning to layer content in SwiftUI.

--- swiftui-practice/CaptionPhoto/CaptionPhoto/CaptionPhoto.swift
+++ swiftui-practice/CaptionPhoto/CaptionPhoto/CaptionPhoto.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CaptionPhoto: View {
     let imageName: String
     let captionText: String
+    
     var body: some View {
         Image(imageName)
             .resizable()
@@ -17,31 +18,32 @@ struct CaptionPhoto: View {
             .overlay(alignment: .bottom) {
                 Caption(text: captionText)
             }
-            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+            .clipShape(RoundedRectangle(cornerRadius: 30.0, style: .continuous))
             .padding()
     }
 }
 
 struct Caption: View {
     let text: String
+    
     var body: some View {
         Text(text)
             .padding()
+            .background(Color.black.opacity(0.75), in: RoundedRectangle(cornerRadius: 10, style: .continuous))
             .foregroundStyle(Color.white)
-            .background(Color.black.opacity(0.75),
-                        in: RoundedRectangle(cornerRadius: 10, style: .continuous))
+            .bold()
             .padding()
     }
 }
 
 struct CaptionPhoto_Previews: PreviewProvider {
     static let landscape = "landscape"
-    static let landscapeCaption = "landscape photo"
+    static let landscapeCaption = "Width is greater than height."
     static let portrait = "portrait"
-    static let portraitPhoto = "portrait photo"
+    static let portraitCaption = "Height is greater than width."
     
     static var previews: some View {
         CaptionPhoto(imageName: landscape, captionText: landscapeCaption)
-        CaptionPhoto(imageName: portrait, captionText: portraitPhoto)
+        CaptionPhoto(imageName: portrait, captionText: portraitCaption)
     }
 }
