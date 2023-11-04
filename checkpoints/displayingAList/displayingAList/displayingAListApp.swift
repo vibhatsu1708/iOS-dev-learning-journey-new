d2cfb7258df395859bb50bf6555853e005c220b7
vibhatsu08 <mistryvedant17@gmail.com>
2023-11-04 21:18:34 +0530

    Completed Coursera's assignment on learning to display a list using an array of items.

---
+++ checkpoints/displayingAList/displayingAList/displayingAListApp.swift
@@ -0,0 +1,17 @@
+//
+//  displayingAListApp.swift
+//  displayingAList
+//
+//  Created by Vedant Mistry on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct displayingAListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
