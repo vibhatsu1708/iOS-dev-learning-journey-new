86d95c2ea2b4b5618dbbe4554e94a5a504d5cc8a
Vedant Mistry <37664832+vibhatsu1708@users.noreply.github.com>
2024-01-06 17:59:09 +0530

    Add files via upload

--- checkpoints/ARRoll/ARRoll/ViewController.swift
+++ checkpoints/ARRoll/ARRoll/ViewController.swift
@@ -22,11 +22,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let sphere = SCNSphere(radius: 0.1)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "art.scnassets/2k_mars.jpg")
+        sphere.materials = [material]
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let node = SCNNode(geometry: sphere)
+        node.position = SCNVector3(x: 0.1, y: 0.1, z: -0.5)
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let carScene = SCNScene(named: "art.scnassets/astonAston MArtinV12 Vantage S 2016.scn")!
+//        if let carNode = carScene.rootNode.childNode(withName: "Car", recursively: true) {
+//            carNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//            sceneView.scene.rootNode.addChildNode(carNode)
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
