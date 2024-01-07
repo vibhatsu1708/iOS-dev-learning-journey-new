bde1ef9d0092a512b03bdb59418d9cf6d8cd8a03
vibhatsu08 <mistryvedant17@gmail.com>
2024-01-08 02:44:31 +0530

    Code snippet for the updated code when the user wants to add objects when the touches begin getting recorded in ARKit.

--- code-snippets/ARKitDisplayObjectWhenTouchesBegan.playground/Contents.swift
+++ code-snippets/ARKitDisplayObjectWhenTouchesBegan.playground/Contents.swift
@@ -1,3 +1,16 @@
-import Cocoa
-
-var greeting = "Hello, playground"
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let location = touches.first?.location(in: sceneView) else {
+        return
+    }
+    guard let query = sceneView.raycastQuery(from: location, allowing: .estimatedPlane, alignment: .horizontal) else {
+        return
+    }
+    guard let result = sceneView.session.raycast(query).first else {
+        return
+    }
+    let ship = SCNScene(named: "art.scnassets/ship.scn")!
+    if let shipNode: SCNNode = ship.rootNode.childNode(withName: "ship", recursively: true) {
+        shipNode.transform = SCNMatrix4(result.worldTransform)
+        sceneView.scene.rootNode.addChildNode(shipNode)
+    }
+}
