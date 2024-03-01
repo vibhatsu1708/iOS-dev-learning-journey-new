ac573c1311fc7b077a08562c76d70fdafa395970
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-02 02:06:32 +0530

    Learnt the basics of OOPS in Swift.

---
+++ BasicsToIntermediate/OOPs.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// structs are value types
+struct MyFirstObject {
+    var title: String = "helloworld"
+}
+
+// classes are reference types
+class MySecondObject {
+    var title: String = "helloworld"
+}
+
