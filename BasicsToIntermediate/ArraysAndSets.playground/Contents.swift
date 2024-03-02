ecbcb816fa766890fe74892ac3c7fdf34aa608e8
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-03 02:48:26 +0530

    Refreshed concepts on Arrays and Sets in Swift.

--- BasicsToIntermediate/ArraysAndSets.playground/Contents.swift
+++ BasicsToIntermediate/ArraysAndSets.playground/Contents.swift
@@ -1,3 +1,16 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+var name1: String = "albert"
+var name2: String = "robert"
+
+struct NameModel {
+    let name1: String
+    let name2: String
+}
+
+func addNames(value: NameModel) {
+    
+}
+addNames(value: NameModel(name1: name1, name2: name2))
+
+let names: [String] = []
