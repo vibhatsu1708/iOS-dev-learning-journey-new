a33e375f0ec037dcd636c37d55c7601abb716418
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-03 17:37:48 +0530

    Learning more about Protocols in Swift

---
+++ BasicsToIntermediate/Protocols.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// A protocol is a list of rules or requirements that an object has to follow.
+
+//struct EmployeeModel: EmployeeHasAName {
+//    let title: String
+//    let name: String
+//}
+//
+//protocol EmployeeHasAName {
+//    let name: String
+//}
