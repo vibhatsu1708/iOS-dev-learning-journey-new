9bb13bd710608d8f36d53042869d6dd2ce6204ac
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-05 01:14:07 +0530

    Learnt about using ignores safe area in SwiftUI

--- SwiftUIBootcampYoutube/SwiftUIBasicsBootcamp.swiftpm/ignoringSafeArea.swift
+++ SwiftUIBootcampYoutube/SwiftUIBasicsBootcamp.swiftpm/ignoringSafeArea.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct ignoringSafeArea: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text("Title goes here")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                ForEach(0..<10) { index in
+                    Rectangle()
+                        .frame(height: 150)
+                        .shadow(radius: 10)
+                        .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                        .padding(20)
+                }
+            }
+        }
+        .background(
+            Color.indigo
+                .ignoresSafeArea()
+        )
+        
+        
+        
+//        VStack {
+//            Text("Ignoring safe area")
+//            Spacer()
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(.indigo)
+//        .ignoresSafeArea(edges: [.leading, .trailing, .bottom])
     }
 }
 
