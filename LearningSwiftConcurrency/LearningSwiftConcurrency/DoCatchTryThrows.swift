39f8462845d53625b74b30965a8c954892b1a217
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-06 01:10:17 +0530

    Learnt about extracting subviews, and making code more readable

---
+++ LearningSwiftConcurrency/LearningSwiftConcurrency/DoCatchTryThrows.swift
@@ -0,0 +1,18 @@
+//
+//  DoCatchTryThrows.swift
+//  LearningSwiftConcurrency
+//
+//  Created by Vedant Mistry on 06/03/24.
+//
+
+import SwiftUI
+
+struct DoCatchTryThrows: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DoCatchTryThrows()
+}
---
+++ LearningSwiftConcurrency/LearningSwiftConcurrency/LearningSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftConcurrencyApp.swift
+//  LearningSwiftConcurrency
+//
+//  Created by Vedant Mistry on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
