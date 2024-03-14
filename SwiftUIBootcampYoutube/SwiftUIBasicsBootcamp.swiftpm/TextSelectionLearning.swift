6901b29f8582bb62644330f47c4c7e17d6d82536
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-15 02:16:27 +0530

    Learnt about text selection in SwiftUI, and how to implement a feature that enables the users to select a certain or a specific text in your app.

---
+++ SwiftUIBootcampYoutube/SwiftUIBasicsBootcamp.swiftpm/TextSelectionLearning.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionLearning.swift
+//  SwiftUIBasicsBootcamp
+//
+//  Created by Vedant Mistry on 15/03/24.
+//
+
+import SwiftUI
+
+struct TextSelectionLearning: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionLearning()
+}
