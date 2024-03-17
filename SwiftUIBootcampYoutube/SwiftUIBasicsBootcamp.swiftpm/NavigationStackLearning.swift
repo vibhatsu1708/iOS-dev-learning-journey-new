8a150b2ce4a2f76f7415f4ad2fc62d258d400925
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-17 17:23:06 +0530

    Learnt about toolbars and how to add buttons, change the default theme or background colors of the toolbar and how to add a menu in the navigation stack toolbar for different actions to be performed.

---
+++ SwiftUIBootcampYoutube/SwiftUIBasicsBootcamp.swiftpm/NavigationStackLearning.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackLearning.swift
+//  SwiftUIBasicsBootcamp
+//
+//  Created by Vedant Mistry on 17/03/24.
+//
+
+import SwiftUI
+
+struct NavigationStackLearning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackLearning()
+}
