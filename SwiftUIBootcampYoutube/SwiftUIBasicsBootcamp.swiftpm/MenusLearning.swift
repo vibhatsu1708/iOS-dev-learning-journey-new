78f78301f0c1cbaf6b4fb3f0b5e2a93b52e37968
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-18 03:31:33 +0530

    Learnt how to create menus, improved pickers, can be used for something like filters etc.

---
+++ SwiftUIBootcampYoutube/SwiftUIBasicsBootcamp.swiftpm/MenusLearning.swift
@@ -0,0 +1,30 @@
+//
+//  MenusLearning.swift
+//  SwiftUIBasicsBootcamp
+//
+//  Created by Vedant Mistry on 18/03/24.
+//
+
+import SwiftUI
+
+struct MenusLearning: View {
+    var body: some View {
+        Menu("Click here") {
+            Button("Button 1") {
+                
+            }
+            
+            Button("Button 2") {
+                
+            }
+            
+            Button("Button 3") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenusLearning()
+}
