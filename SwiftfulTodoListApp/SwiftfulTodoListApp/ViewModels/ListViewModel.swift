334293f8e109fa52627beb85bdf447a9adab23ad
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-19 03:26:45 +0530

    Implemented UserDefaults to save the data to JSON, and then fetch it back whenever any CRUD operation is performed.

--- SwiftfulTodoListApp/SwiftfulTodoListApp/ViewModels/ListViewModel.swift
+++ SwiftfulTodoListApp/SwiftfulTodoListApp/ViewModels/ListViewModel.swift
@@ -8,20 +8,24 @@
 import Foundation
 
 class ListViewModel: ObservableObject {
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title", isCompleted: false),
-            ItemModel(title: "This is the second title", isCompleted: true),
-            ItemModel(title: "This is the third title", isCompleted: false)
-        ]
-        
-        items.append(contentsOf: newItems)
+        guard 
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        self.items = savedItems
     }
     
     func completeItem(item: ItemModel) {
@@ -41,5 +45,11 @@ class ListViewModel: ObservableObject {
     
     func moveItem(from: IndexSet, to: Int) {
         items.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
     }
 }
