956e2f51b4366f3878b9fc55d7f24fb730d2a97a
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-21 01:21:27 +0530

    Learnt aboutt the Drag Gesture, and learnt how to animate views using the drag gesture and make a custom pop up screen or a pop up sheet, using the drag gesture, by pulling it down and pulling it up.

--- SwiftUIContinuedLearning/DragGestureLearning.swift
+++ SwiftUIContinuedLearning/DragGestureLearning.swift
@@ -8,17 +8,59 @@
 import SwiftUI
 
 struct DragGestureLearning: View {
+    @State var offset: CGSize = .zero
+    @State var onContact: Bool = false
+    
     var body: some View {
-        RoundedRectangle(cornerRadius: 20.0)
-            .frame(width: 200, height: 200)
-            .foregroundStyle(Color.indigo)
-            .padding()
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        
-                    }
+        VStack {
+            VStack {
+                Text("\(offset.width)")
+            }
+            
+            Spacer()
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .frame(width: 300, height: 600)
+                .foregroundStyle(Color.indigo)
+    //            .scaleEffect(onContact ? 1.5 : 1.0)
+                .scaleEffect(getScaleAmount())
+                .rotationEffect(Angle(degrees: getRotationAmount()))
+                .padding()
+                .offset(offset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(Animation.spring) {
+                                offset = value.translation
+    //                            onContact = true
+                            }
+                        }
+                        .onEnded { value in
+                            withAnimation(Animation.spring) {
+                                offset = .zero
+    //                            onContact = false
+                            }
+                        }
             )
+        }
+    }
+    
+    func getScaleAmount() -> CGFloat {
+        let maxAmount = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / maxAmount
+        
+        return 1.0 - min(percentage, 0.5) * 0.5
+    }
+    
+    func getRotationAmount() -> Double {
+        let maxAmount = UIScreen.main.bounds.width / 2
+        let currentAmount = offset.width
+        let percentage = currentAmount / maxAmount
+        let percentageAsDouble = Double(percentage)
+        let maxAngle: Double = 10
+        
+        return percentageAsDouble * maxAngle
     }
 }
 
--- SwiftUIContinuedLearning/DragGestureLearning2.swift
+++ SwiftUIContinuedLearning/DragGestureLearning2.swift
@@ -8,11 +8,79 @@
 import SwiftUI
 
 struct DragGestureLearning2: View {
+    @State var startingOffsetY: CGFloat = UIScreen.main.bounds.height * 0.85
+    @State var currentDragOffsetY: CGFloat = 0
+    @State var endingDragOffsetY: CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.yellow
+                .ignoresSafeArea()
+            
+            SignUpView()
+                .offset(y: startingOffsetY)
+                .offset(y: currentDragOffsetY)
+                .offset(y: endingDragOffsetY)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(Animation.spring) {
+                                currentDragOffsetY = value.translation.height
+                            }
+                        }
+                        .onEnded { value in
+                            withAnimation(Animation.spring) {
+                                if currentDragOffsetY < -150 {
+                                    endingDragOffsetY = -startingOffsetY
+                                } else if endingDragOffsetY != 0 && currentDragOffsetY > -150 {
+                                    endingDragOffsetY = 0
+                                }
+                                currentDragOffsetY = 0
+                            }
+                            
+                            withAnimation(Animation.spring) {
+                                currentDragOffsetY = 0
+                            }
+                        }
+                )
+        }
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
 #Preview {
     DragGestureLearning2()
+}
+
+struct SignUpView: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            Image(systemName: "chevron.up")
+                .padding(.top)
+            
+            Text("Sign up")
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            Image(systemName: "flame.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 100)
+            
+            Text("This is the description. SwiftUI is the future of ios development.")
+                .multilineTextAlignment(.center)
+            
+            Text("Create an account")
+                .padding()
+                .foregroundStyle(Color.white)
+                .background(Color.black)
+                .clipShape(RoundedRectangle(cornerRadius: 20.0))
+            
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 40.0))
+    }
 }
