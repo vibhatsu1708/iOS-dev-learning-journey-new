e6f74dbabe1829302495c69959d1a66b990d911e
vibhatsu08 <mistryvedant17@gmail.com>
2024-03-22 16:12:11 +0530

    Learned about the Hashable property in SwiftUI, and how and when to use it over Identifiable, and how to create the unique hashes for values in the array, using the components.

---
+++ SwiftUIContinuedLearning/HashableLearning.swift
@@ -0,0 +1,41 @@
+//
+//  HashableLearning.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Vedant Mistry on 22/03/24.
+//
+
+import SwiftUI
+
+struct MyCustomModel: Hashable {
+    let title: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(title)
+    }
+}
+
+struct HashableLearning: View {
+    let data: [MyCustomModel] = [
+        MyCustomModel(title: "One"),
+        MyCustomModel(title: "Two"),
+        MyCustomModel(title: "Three"),
+        MyCustomModel(title: "Four"),
+        MyCustomModel(title: "Five")
+    ]
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 40) {
+                ForEach(data, id: \.self) { item in
+                    Text(item.hashValue.description)
+                        .font(.headline)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HashableLearning()
+}
