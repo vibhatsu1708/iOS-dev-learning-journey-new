37ff3c797f277379996b86d79f38f5ad39172bb5
vibhatsu08 <mistryvedant17@gmail.com>
2024-04-01 18:01:06 +0530

    Learn to download data from a url using Combine.

---
+++ SwiftUIContinuedLearning/DownloadJsonWithCombineLearning.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadJsonWithCombineLearning.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Vedant Mistry on 01/04/24.
+//
+
+import SwiftUI
+
+struct DownloadJsonWithCombineLearning: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadJsonWithCombineLearning()
+}
