da0b6ed771821f433ecdc823e7f3de939a090fc7
vibhatsu08 <mistryvedant17@gmail.com>
2024-04-02 00:19:06 +0530

    Learnt about timers and the onReceive, and how to use the timer publisher to create some simple loading animations, page carousel type automatic views.

--- SwiftUIContinuedLearning/TimersAndOnReceiveLearning.swift
+++ SwiftUIContinuedLearning/TimersAndOnReceiveLearning.swift
@@ -9,7 +9,8 @@ import SwiftUI
 import Combine
 
 struct TimersAndOnReceiveLearning: View {
-    let timer = Timer.publish(every: 1.0,
+    // this timer is a publisher, basically publishes a value over a certain time span, over time
+    let timer = Timer.publish(every: 3.0,
                               on: .main,
                               in: .common).autoconnect()
     
@@ -31,6 +32,7 @@ struct TimersAndOnReceiveLearning: View {
      */
     
     // countdown to date
+    /*
     @State var timeRemaining: String = ""
     let futureDate: Date = Calendar.current.date(byAdding: .hour,
                                                  value: 1,
@@ -46,6 +48,10 @@ struct TimersAndOnReceiveLearning: View {
         timeRemaining = "\(hour): \(minute): \(second)"
         
     }
+     */
+    
+    // animation counter
+    @State var count = 1
     
     var body: some View {
         ZStack {
@@ -57,13 +63,59 @@ struct TimersAndOnReceiveLearning: View {
             
 //            Text(dateFormatter.string(from: currentDate))
 //            Text(finishedText ?? "\(count)")
-            Text(timeRemaining)
-                .font(.system(size: 30))
-                .fontWeight(.heavy)
-                .foregroundStyle(Color.white)
+//            Text(timeRemaining)
+//                .font(.system(size: 30))
+//                .fontWeight(.heavy)
+//                .foregroundStyle(Color.white)
+            
+//            HStack(spacing: 20) {
+//                Circle()
+//                    .offset(y: count == 1 ? -40 : 0)
+//                Circle()
+//                    .offset(y: count == 2 ? -40 : 0)
+//                Circle()
+//                    .offset(y: count == 3 ? -40 : 0)
+//            }
+//            .frame(width: 150)
+//            .foregroundStyle(Color.white)
+            
+            TabView(selection: $count,
+                    content:  {
+                Rectangle()
+                    .foregroundStyle(Color.green)
+                    .tag(1)
+                Rectangle()
+                    .foregroundStyle(Color.purple)
+                    .tag(2)
+                Rectangle()
+                    .foregroundStyle(Color.yellow)
+                    .tag(3)
+                Rectangle()
+                    .foregroundStyle(Color.orange)
+                    .tag(4)
+                Rectangle()
+                    .foregroundStyle(Color.teal)
+                    .tag(5)
+                Rectangle()
+                    .foregroundStyle(Color.blue)
+                    .tag(6)
+                Rectangle()
+                    .foregroundStyle(Color.mint)
+                    .tag(7)
+                Rectangle()
+                    .foregroundStyle(Color.pink)
+                    .tag(8)
+            })
+            .padding()
+            .frame(height: 200)
+            .clipShape(RoundedRectangle(cornerRadius: 10.0))
+            .tabViewStyle(PageTabViewStyle())
         }
         .onReceive(timer, perform: { _ in
-            updateTimeRemaining()
+//            updateTimeRemaining()
+            withAnimation(.default) {
+                count = (count == 8) ? 1 : (count + 1)
+            }
         })
     }
 }
