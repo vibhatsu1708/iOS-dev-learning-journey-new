5a9e48781f97ba4d7b929a7a848dc615de7b1c07
vibhatsu08 <mistryvedant17@gmail.com>
2024-04-22 03:37:27 +0530

    Learnt about the strong and the weak references, and how to manage updating and cancelling tasks by using onAppear and onDisappear, so that the viewmodel is automatically deallocated when the view disappears and to ensure that it doesn't keep running in the background until actually that is needed. Also learnt that the onAppear and the onDisappear for cancelling the tasks can be handled in a much more efficient way using the .task modifier on the view where it is needed, so then you wouldn't need to actually maintain a reference to that task. And when using MVVM architecture you might use the .task or choose to maintain the references for the task or the tasks which are running either singular or in an array where you can loop through to cancel all the tasks in that array.

---
+++ LearningSwiftConcurrency/AsyncAwaitStrongWeakReferencesLearning.swift
@@ -0,0 +1,116 @@
+//
+//  AsyncAwaitStrongWeakReferencesLearning.swift
+//  LearningSwiftConcurrency
+//
+//  Created by Vedant Mistry on 22/04/24.
+//
+
+import SwiftUI
+
+final class DataService {
+    func getData() async -> String {
+        return "Updated data!"
+    }
+}
+
+final class StrongWeakReferencesViewModel: ObservableObject {
+    var manager = DataService()
+    @Published var data: String = "Some title!"
+    
+    // to be able to deal with cancelling tasks without using strong and weak references, you can do something like this.
+    // basically creating a dummy task and setting it equal to that particular task or to all the tasks.
+    private var someTask: Task<Void, Never>? = nil
+    
+    // what if you had an array of tasks, you could do this.
+    private var someTasks: [Task<Void, Never>] = []
+    
+    // with a function to cancel the task
+    func cancelTasks() {
+        // can do either this
+        someTask?.cancel()
+        // or this
+        someTask = nil
+        
+        
+        
+        // for cancelling multiple tasks in the array
+        // basically first cancelling all the tasks and then setting the array to empty.
+        someTasks.forEach({ $0.cancel() })
+        someTasks = []
+    }
+    
+    func updateData1() {
+        Task {
+            // This implies a strong reference.
+            // you can add "self.data", but its mostly used when you are dealing with a parameter that is also data and you want to make it clear which data are you referring to. "self.data" refers to the particular data from the braces, of that function or class or whatever.
+            data = await manager.getData()
+        }
+    }
+    
+    func updateData2() {
+        Task {
+            // This implies a strong reference.
+            // with the "self.data"
+            self.data = await manager.getData()
+        }
+    }
+    
+    func updateData3() {
+        Task { [self] in
+            // This also is a strong reference.
+            self.data = await manager.getData()
+        }
+    }
+    
+    func updateData4() {
+        Task { [weak self] in
+            // This implies a weak reference.
+            if let data = await self?.manager.getData() {
+                self?.data = data
+            }
+        }
+    }
+    
+    
+    // we dont need to manage weak and strong references because we can manage the task itself.
+    func updateData5() {
+        someTask = Task {
+            self.data = await manager.getData()
+        }
+    }
+    
+    func updateData6() {
+        let task1 = Task {
+            self.data = await manager.getData()
+        }
+        someTasks.append(task1)
+        
+        let task2 = Task {
+            self.data = await manager.getData()
+        }
+        someTasks.append(task1)
+        
+        let task3 = Task {
+            self.data = await manager.getData()
+        }
+        someTasks.append(task1)
+    }
+}
+
+struct AsyncAwaitStrongWeakReferencesLearning: View {
+    @StateObject var viewModel = StrongWeakReferencesViewModel()
+    
+    var body: some View {s̄
+        Text(viewModel.data)
+            .onAppear {
+                viewModel.updateData1()
+            }
+            .onDisappear {
+                viewModel.cancelTasks()
+            }
+    }
+}
+
+#Preview {
+    AsyncAwaitStrongWeakReferencesLearning()
+}
