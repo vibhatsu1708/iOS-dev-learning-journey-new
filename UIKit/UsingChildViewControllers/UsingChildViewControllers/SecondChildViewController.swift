85b7a724167c2645ea0232283557b4e06179d6f8
vibhatsu08 <mistryvedant17@gmail.com>
2024-07-01 00:29:39 +0530

    Learnt how to use child view container and place them in container views.

---
+++ UIKit/UsingChildViewControllers/UsingChildViewControllers/SecondChildViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  UsingChildViewControllers
+//
+//  Created by Vedant Mistry on 01/07/24.
+//
+
+import UIKit
+
+class SecondChildViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
--- UIKit/UsingChildViewControllers/UsingChildViewControllers/ViewController.swift
+++ UIKit/UsingChildViewControllers/UsingChildViewControllers/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let secondChildVC = SecondChildViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addSecondChildVC()
     }
-
-
+    
+    func addSecondChildVC() {
+        addChild(SecondChildViewController())
+        view.addSubview(secondChildVC.view)
+        secondChildVC.view.backgroundColor = .blue
+        secondChildVC.didMove(toParent: self)
+        setSecondChildVCConstraints()
+    }
+    
+    func setSecondChildVCConstraints() {
+        secondChildVC.view.translatesAutoresizingMaskIntoConstraints = false
+        secondChildVC.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        secondChildVC.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        secondChildVC.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        secondChildVC.view.heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
 }
 
